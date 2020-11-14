3fee20ea81f065d0025d04e55742c177afa46b92
Alannah Kearney <hi@alannahkearney.com>
2020-11-15 09:29:49 +1000

    feat: Create remaining asset type iterator classes

---
+++ src/ExtensionAssetManagement/Iterators/ContentIterator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Symphony CMS: Extension Asset Management" repository.
+ *
+ * Copyright 2020 Alannah Kearney <hi@alannahkearney.com>
+ *
+ * For the full copyright and license information, please view the LICENCE
+ * file that was distributed with this source code.
+ */
+
+namespace pointybeard\Symphony\ExtensionAssetManagement\Iterators;
+
+use pointybeard\Symphony\ExtensionAssetManagement;
+
+class ContentIterator extends AbstractAssetIterator
+{
+    public function current(): ExtensionAssetManagement\AbstractInstallableAsset
+    {
+        $file = (object) parent::current();
+
+        return ExtensionAssetManagement\AssetFactory::build('Content', $file->name);
+    }
+}
