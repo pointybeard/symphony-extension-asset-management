ee64d6260889779cb932e4e96a63426cd96a1453
Alannah Kearney <hi@alannahkearney.com>
2020-11-15 09:26:31 +1000

    feat: Add SymbolicLinkCreationFailedException exception

---
+++ src/ExtensionAssetManagement/Exceptions/SymbolicLinkCreationFailedException.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "Symphony CMS: Extension Asset Management" repository.
+ *
+ * Copyright 2020 Alannah Kearney <hi@alannahkearney.com>
+ *
+ * For the full copyright and license information, please view the LICENCE
+ * file that was distributed with this source code.
+ */
+
+namespace pointybeard\Symphony\ExtensionAssetManagement\Exceptions;
+
+class SymbolicLinkCreationFailedException extends ExtensionAssetManagementException
+{
+    public function __construct(string $name, string $message, int $code = 0, \Exception $previous = null)
+    {
+        parent::__construct("Faild to create sybmolic link for '{$name}'. Returned: {$message}", $code, $previous);
+    }
+}
